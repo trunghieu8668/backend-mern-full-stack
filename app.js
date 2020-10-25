b45e2b1ca479c7374fe87b6ea2a5035117eb07f1
trunghieu8668 <64822814+trunghieu8668@users.noreply.github.com>
2020-10-25 15:45:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -58,8 +58,11 @@ if(process.env.NODE_ENV == 'production'){
   app.use(express.static('backend/build'))
   const path = require('path')
  
-  app.get("*", (req, res) => {
+  app.get("/admin", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'backend', 'build', 'index.html'))
+  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
   
 }
